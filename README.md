52fd851534ac6bbfebe6fec3a92cd2c884d89fd4
mel-rossi <melani.r.rodrigues@gmail.com>
2026-05-06 00:16:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# gmaps-api-game 
+
+COMP 484 - Project 5: Google Maps API - JS Game 
+
+[GitHub Pages Link](https://mel-rossi.github.io/gmaps-api-game/)
+
+[Google Maps Platform Docs - Demo Key](https://developers.google.com/maps/documentation/javascript/demo-key)
+
+## Configure Google Maps API
+
+1. Rename the file `config.template.js` to `config.js`
+2. Replace `API_KEY_HERE` with your Google Maps API key
+   - Demo Key can be obtained in the Link provided above
+3. Open `index.html` in your web browser of choice
---
+++ config.template.js
@@ -0,0 +1,14 @@
+const API_Key = "API_KEY_HERE"; // Replace API_KEY_HERE with your own Google Maps API Key
+
+// Dynamically inject the Google Maps script using the key
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
+
+// Rename this file to config.js
+// Do not change the name of the script reference inside the HTML 
+// to config.template.js instead 
+// config.js is inside .gitignore, which will prevent your API_KEY from being publically 
+// available on GitHub if you push any code
