85e6498d5f6a0c208ee225049ca0a3e8955b54ac
mel-rossi <melani.r.rodrigues@gmail.com>
2026-05-07 12:21:08 -0700

    Revamp Load Question & Game Panel into Log + Functioning: Next Question & End Game Panel

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 let map; // Map
 let question = 0; // Question number
-const TOLERANCE = 300; // in meters
+const TOLERANCE = 3000; // in meters
+const mapObj = document.getElementById("map"); // Map Object
+const startBtn = document.getElementById("start-btn"); // Start Button
+const gamePanel = document.getElementById("game-panel"); // Game Panel
+const introPanel = document.getElementById("intro-panel"); // Intro Panel
+const logBox = document.getElementById("log-box");
+const feedbackBox = document.getElementById("feedback-box"); // Feedback Box
+const resultsPanel = document.getElementById("results-panel"); // Results Panel
 
 const LOCATIONS = [ // Locations
     { name: "Black House",                 grid: "B6", lat: 34.2436, lng: -118.5233 },
@@ -14,7 +21,7 @@ const LOCATIONS = [ // Locations
 
 // Map Initialization 
 function initMap() { 
-    map = new google.maps.Map(document.getElementById("map"), { 
+    map = new google.maps.Map(mapObj, { 
         center: { lat: 34.239294, lng: -118.529317 }, // CSUN Campus
         zoom: 17, 
         mapTypeId: "roadmap",
@@ -58,9 +65,9 @@ function initMap() {
 
 // Start Playing
 function startGame() { 
-    document.getElementById("intro-panel").close(); // Close Intro Panel
-    document.getElementById("map").removeAttribute("hidden"); // Open Map
-    document.getElementById("game-panel").removeAttribute("hidden"); // Open Game Panel
+    introPanel.close(); // Close Intro Panel
+    // mapObj.removeAttribute("hidden"); // Open Map
+    gamePanel.removeAttribute("hidden"); // Open Game Panel
 
     loadQuestion(question) // Load first question 
 }
@@ -68,10 +75,33 @@ function startGame() {
 // Load question 
 function loadQuestion(i) { 
     const loc = LOCATIONS[i]; 
-    document.getElementById("building-name").innerHTML = loc.name;
+
+    // Question Block 
+    const block = document.createElement("div");
+    block.className = "panel prompt-box";
+    block.id = `question-${i}`;
+    block.innerHTML = `
+        <h2 id="prompt-label">Where is <span id="building-name">${loc.name}</span>?</h2>
+
+        <button class="next-btn", id="${i}-next-btn">Next</button>
+    `;
+    
+    // Append Question block to log box 
+    logBox.appendChild(block); 
+
+    // Disable Next Button on all questions 
+    const allNextBtns = document.querySelectorAll(".next-btn"); 
+    allNextBtns.forEach(btn => btn.disabled = true); 
+
+    // Enable Next Button on latest question 
+    block.querySelector(".next-btn").disabled = false; 
+    block.querySelector(".next-btn").addEventListener("click", nextQuestion);
+
+    // Scroll to latest question 
+    block.scrollIntoView({ behavior: "smooth" });
 }
 
-// Double Click on Map
+// Double Click on Map (Come back when Demo Key limit resets)
 function handleClick(clickedLatLng) { 
     const loc = LOCATIONS[question];
     const correctLatLng = new google.maps.LatLng(loc.lat, loc.lng);
@@ -89,8 +119,26 @@ function handleClick(clickedLatLng) {
     }
 }
 
+// Move to Next Question 
+function nextQuestion() { 
+    this.disabled = true; // Disable clicked Next button 
+    question++; 
+
+    // If all questions have been answered End the Game
+    if (question >= LOCATIONS.length) { 
+        endGame();
+    } else { // Else move to the next question
+        loadQuestion(question);
+    }
+}
+
+// End Game 
+function endGame() { 
+    gamePanel.setAttribute("hidden", ""); // Hide Game Panel
+    resultsPanel.show(); // Open Results Panel
+}
+
 // Event Listeners 
 
 // Start Button 
-document.getElementById("start-btn").addEventListener("click", startGame);
-
+startBtn.addEventListener("click", startGame);
