60903f0c1125acacaca9c18bc5ec8224440e964f
mel-rossi <melani.r.rodrigues@gmail.com>
2026-05-06 23:01:43 -0700

    Add Geometry Library

--- config.template.js
+++ config.template.js
@@ -2,7 +2,7 @@ const API_Key = "API_KEY_HERE"; // Replace API_KEY_HERE with your own Google Map
 
 // Dynamically inject the Google Maps script using the key
 const script = document.createElement("script");
-script.src = `https://maps.googleapis.com/maps/api/js?key=${API_Key}&callback=initMap&loading=async`;
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_Key}&libraries=geometry&callback=initMap&loading=async`;
 script.defer = true;
 document.head.appendChild(script);
 
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // Global Variables 
-const LOCATIONS = [ // Locations
-    { name: "Black House", grid: "B6" },
-    { name: "Santa Susana Hall", grid: "D2" },
-    { name: "C.R. Johnson Auditorium", grid: "D5" }, 
-    { name: "Student Recreation Center", grid: "G4" },
-    { name: "Extended University Commons", grid: "B4" },
-];
 
+let map; // Map
 let question = 0; // Question number
+const TOLERANCE: 60; // in meters
+
+const LOCATIONS = [ // Locations
+    { name: "Black House",                 grid: "B6", lat: 34.2436, lng: -118.5233 },
+    { name: "Santa Susana Hall",           grid: "D2", lat: 34.2446, lng: -118.5298 },
+    { name: "C.R. Johnson Auditorium",     grid: "D5", lat: 34.2417, lng: -118.5251 },
+    { name: "Student Recreation Center",   grid: "G4", lat: 34.2381, lng: -118.5262 },
+    { name: "Extended University Commons", grid: "B4", lat: 34.2440, lng: -118.5263 },
+];
 
 // Map Initialization 
 function initMap() { 
-    const map = new google.maps.Map(document.getElementById("map"), { 
+    map = new google.maps.Map(document.getElementById("map"), { 
         center: { lat: 34.2405, lng: -118.5278 }, // CSUN Campus 
         zoom: 17, 
         mapTypeId: "roadmap",
@@ -63,8 +66,31 @@ function loadQuestion(i) {
     document.getElementById("building-name").innerHTML = loc.name;
 }
 
+// Double Click on Map
+function handleClick(clickedLatLng) { 
+    const loc = LOCATIONS[question];
+    const correctLatLng = new google.maps.latLng(loc.lat, loc.lng);
+
+    // Calculate distance between click and correct location 
+    const distance = google.maps.geometry.spherical, computeDistanceBetween(
+        clickedLatLng,
+        correctLatLng
+    );
+
+    if (distance <= TOLERANCE) { 
+        console.log("Correct!");
+    } else { 
+        console.log("Wrong!");
+    }
+}
+
 // Event Listeners 
 
 // Start Button 
 document.getElementById("start-btn").addEventListener("click", startGame);
+
+// Double Click Event
+map.addListener("dbclick", function(e) { 
+    handleClick(event.latLng);
+});
 
