61077d5a82641c29879cdee535a78a9555628eca
mel-rossi <melani.r.rodrigues@gmail.com>
2026-05-11 00:39:04 -0700

    Deployment Preparations

--- config.template.js
+++ config.template.js
@@ -1,13 +1,7 @@
-const API_Key = "API_KEY_HERE"; // Replace API_KEY_HERE with your own Google Maps API Key
+const API_Key = "%%API_KEY%%";
 
 // Dynamically inject the Google Maps script using the key
 const script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=${API_Key}&libraries=geometry&callback=initMap&loading=async`;
 script.defer = true;
 document.head.appendChild(script);
-
-// Rename this file to config.js
-// Do not change the name of the script reference inside the HTML 
-// to config.template.js instead 
-// config.js is inside .gitignore, which will prevent your API_KEY from being publically 
-// available on GitHub if you push any code
